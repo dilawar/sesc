ba1ed071d91e4173018b9a66042c80634bda30b6
Dilawar Singh <dilawars@ncbs.res.in>
2019-01-27 16:55:07 +0530

    Adding -m32 to list.

--- src/libcore/MemObj.h
+++ src/libcore/MemObj.h
@@ -92,9 +92,9 @@ public:
   const LevelType *getLowerLevel() const { return &lowerLevel; }
   const LevelType *getUpperLevel() const { return &upperLevel; }
 
-  const uint32_t getUpperLevelSize() const { return upperLevel.size(); }
+  uint32_t getUpperLevelSize() const { return upperLevel.size(); }
 
-  virtual const bool isCache() const { return false; }
+  virtual bool isCache() const { return false; }
 
   const uint32_t getNumCachesInUpperLevels() const {
     return nUpperCaches;
